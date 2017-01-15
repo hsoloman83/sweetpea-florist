1e27950ea3515b74e749c404d4de7bf231d04779
hsoloman83 <hanna.soloman@gmail.com>
2017-01-15 17:31:49 +0000

    css

--- js/materialize.js
+++ js/materialize.js
@@ -6860,60 +6860,11 @@ Picker.extend( 'pickadate', DatePicker )
           }
           return true;
         });
-
-
-
         window.onresize = scroll;
-
         setupEvents();
         scroll(offset);
-
-        $(this).on('carouselNext', function(e, n) {
-          if (n === undefined) {
-            n = 1;
-          }
-          target = offset + dim * n;
-          if (offset !== target) {
-            amplitude = target - offset;
-            timestamp = Date.now();
-            requestAnimationFrame(autoScroll);
-          }
-        });
-
-        $(this).on('carouselPrev', function(e, n) {
-          if (n === undefined) {
-            n = 1;
-          }
-          target = offset - dim * n;
-          if (offset !== target) {
-            amplitude = target - offset;
-            timestamp = Date.now();
-            requestAnimationFrame(autoScroll);
-          }
-        });
-
       });
-
-
-
-    },
-    next : function(n) {
-      $(this).trigger('carouselNext', [n]);
-    },
-    prev : function(n) {
-      $(this).trigger('carouselPrev', [n]);
     },
   };
-
-
-    $.fn.carousel = function(methodOrOptions) {
-      if ( methods[methodOrOptions] ) {
-        return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-      } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
-        // Default to "init"
-        return methods.init.apply( this, arguments );
-      } else {
-        $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.carousel' );
-      }
-    }; // Plugin end
+ // Plugin end
 }( jQuery ));
